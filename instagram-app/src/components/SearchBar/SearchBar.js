48a5e496fcf0c9bc8708c7a794379abcbbcd54d0
angel prats <angelpratz@gmail.com>
2019-05-13 17:41:38 -0400

    search bar and ig logos rendered on browser

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -3,7 +3,15 @@ import React from 'react';
 const SearchBar = () => {
   return (
     <div>
-      <h1>this is my searchbar</h1>
+      <div>
+        <i class="fab fa-instagram"></i>
+        {/* <img className="ig-script" src="http://www.stickpng.com/assets/images/5a4e432a2da5ad73df7efe7a.png" alt="logo script" /> */}
+
+        <input type="text" />
+        <i class="far fa-compass"></i>
+        <i class="far fa-user"></i>
+        <i class="far fa-heart"></i>
+      </div>
     </div>
   )
 }
